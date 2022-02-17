ef88bebe09dda0e4024e99fab1b01c54f4c4b832
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-02-18 03:47:43 +0900

    Feat: LoginPage 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
 	return (
-		<div className="App">
-			<MainPage />
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<MainPage />} />
+				<Route path="/login" element={<LoginPage />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import styled from 'styled-components';
+import { styles } from '../styles';
+
+const Container = styled.div`
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Wrapper = styled.div`
+	width: 60%;
+	height: 400px;
+	display: flex;
+`;
+
+const Left = styled.div`
+	flex: 1;
+	display: flex;
+	align-items: center;
+`;
+
+const Logo = styled.div`
+	font-family: 'Roboto', sans-serif;
+	font-size: 54px;
+	font-weight: 700;
+	color: ${styles.themeColor};
+`;
+
+const Right = styled.div`
+	flex: 1;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const LoginContainer = styled.div``;
+
+const LoginForm = styled.form`
+	display: flex;
+	flex-direction: column;
+`;
+
+const LoginInput = styled.input`
+	width: 220px;
+	height: 30px;
+	margin-bottom: 5px;
+	outline: none;
+`;
+
+const LoginBtn = styled.button`
+	height: 34px;
+	border: none;
+	background-color: ${styles.themeColor};
+	color: ${styles.whiteColor};
+	font-size: 16px;
+	font-weight: 400;
+	cursor: pointer;
+
+	&:hover {
+		filter: brightness(110%);
+	}
+`;
+
+const LoginPage = () => {
+	const handleLogin = (e) => {
+		e.preventDefault();
+	};
+
+	return (
+		<Container>
+			<Wrapper>
+				<Left>
+					<Logo>LINKBOOK</Logo>
+				</Left>
+				<Right>
+					<LoginContainer>
+						<LoginForm onSubmit={handleLogin}>
+							<LoginInput placeholder="이메일" type="email" />
+							<LoginInput placeholder="비밀번호" type="password" />
+							<LoginBtn type="submit">로그인</LoginBtn>
+						</LoginForm>
+					</LoginContainer>
+				</Right>
+			</Wrapper>
+		</Container>
+	);
+};
+
+export default LoginPage;
